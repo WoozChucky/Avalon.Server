a8b53be107497609a8621ac7ebcba4d86502df0c
Nuno Silva <nuno.levezinho@live.com.pt>
2023-03-26 23:52:35 +0100

    Added more Dispose pattern work

--- Avalon.Network.Tcp/AvalonTcpServer.cs
+++ Avalon.Network.Tcp/AvalonTcpServer.cs
@@ -1,5 +1,9 @@
+using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using Avalon.Network.Tcp.Configuration;
 
 namespace Avalon.Network.Tcp;
@@ -9,15 +13,21 @@ public class AvalonTcpServer : IAvalonNetworkServer
     private readonly AvalonTcpServerConfiguration _configuration;
     private readonly X509Certificate2 _certificate;
     private readonly Socket _socket;
-    private readonly CancellationTokenSource _cts;
     
+    private volatile CancellationTokenSource _cts;
     private volatile bool _isRunning;
 
-    public AvalonTcpServer(AvalonTcpServerConfiguration configuration, CancellationTokenSource? cts = default)
+    public AvalonTcpServer(AvalonTcpServerConfiguration configuration)
     {
         _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-        _cts = cts ?? new CancellationTokenSource();
+        _cts = new CancellationTokenSource();
         _isRunning = false;
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _socket.Bind(new IPEndPoint(IPAddress.Any, _configuration.ListenPort));
+        var serverCertBytes = File.ReadAllBytesAsync(
+                _configuration.CertificatePath ?? throw new ArgumentNullException(nameof(configuration.CertificatePath))
+                ).ConfigureAwait(true).GetAwaiter().GetResult();
+        _certificate = new X509Certificate2(serverCertBytes, _configuration.CertificatePassword);
     }
     
     ~AvalonTcpServer()
@@ -25,11 +35,26 @@ public class AvalonTcpServer : IAvalonNetworkServer
         Dispose(false);
     }
     
-    public Task RunAsync(bool blocking = true)
+    public bool IsRunning => _isRunning;
+    
+    public async Task RunAsync(bool blocking = true)
     {
         if (_isRunning)
         {
             throw new InvalidOperationException("Server is already running.");
+        }
+        
+        _cts = _cts.Token.IsCancellationRequested ? new CancellationTokenSource() : _cts;
+        _socket.Listen(_configuration.Backlog);
+        _isRunning = true;
+        
+        if (blocking)
+        {
+            await InternalServerLoop();
+        }
+        else
+        {
+            await Task.Factory.StartNew(InternalServerLoop).ConfigureAwait(false);
         }
     }
 
@@ -48,8 +73,67 @@ public class AvalonTcpServer : IAvalonNetworkServer
         
         return Task.CompletedTask;
     }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 
-    public bool IsRunning => _isRunning;
+    private async Task InternalServerLoop()
+    {
+        try
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                var client = await _socket.AcceptAsync(_cts.Token);
+                await HandleNewConnection(client).ConfigureAwait(false);
+            }
+        }
+        catch (OperationCanceledException e)
+        {
+            //TODO: Gracefully exit the loop when cancellation is requested
+        }
+    }
+
+    private async Task HandleNewConnection(Socket client)
+    {
+        var sslStream = new SslStream(new NetworkStream(client), false, OnClientCertificateValidation);
+        try
+        {
+            await sslStream.AuthenticateAsServerAsync(_certificate, true, SslProtocols.Tls12, true);
+
+            // Receive a response from the server
+            var buffer = new byte[1024];
+            var bytesRead = await sslStream.ReadAsync(buffer, _cts.Token);
+            if (bytesRead == 0)
+            {
+                // Remote client disconnected
+                return;
+            }
+
+            var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine(response);
+        }
+        catch (AuthenticationException e)
+        {
+            Console.WriteLine($"Failed to authenticate: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to communicate: {e.Message}");
+        }
+        finally
+        {
+            await sslStream.DisposeAsync();
+        }
+    }
+
+    private bool OnClientCertificateValidation(object sender, X509Certificate? certificate, X509Chain? chain, SslPolicyErrors sslpolicyerrors)
+    {
+        return true;
+    }
 
     private void Dispose(bool disposing)
     {
@@ -59,11 +143,5 @@ public class AvalonTcpServer : IAvalonNetworkServer
             _socket.Dispose();
             _cts.Dispose();
         }
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
     }
 }
