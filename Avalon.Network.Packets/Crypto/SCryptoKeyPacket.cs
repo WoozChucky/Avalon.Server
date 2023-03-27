2cbab8b4b870bbb804b8a78b73f4bc1ad3af09d2
Nuno Silva <nuno.levezinho@live.com.pt>
2023-03-27 17:23:35 +0100

    Added more logic for the PacketGenerator registration

--- Avalon.Network.Packets/Crypto/SCryptoKeyPacket.cs
+++ Avalon.Network.Packets/Crypto/SCryptoKeyPacket.cs
@@ -5,5 +5,6 @@ namespace Avalon.Network.Packets.Crypto;
 [ProtoContract]
 public class SCryptoKeyPacket
 {
+    public static NetworkPacketType PacketType = NetworkPacketType.SMSG_ENCRYPTION_KEY;
     [ProtoMember(1)] public byte[] Key { get; set; }
 }
--- Avalon.Network.Packets/PacketGenerator.cs
+++ Avalon.Network.Packets/PacketGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Reflection;
 using Avalon.Network.Packets.Crypto;
 using ProtoBuf;
 
@@ -7,11 +8,13 @@ namespace Avalon.Network.Packets;
 public class PacketGenerator
 {
     
-    private readonly ConcurrentDictionary<NetworkPacketType, Func<object, byte[]>> _packetFactories;
+    private const string PacketTypeFieldName = "PacketType";
+    
+    private readonly ConcurrentDictionary<NetworkPacketType, Func<byte[], object>> _packetDeserializerFactories;
 
     public PacketGenerator()
     {
-        _packetFactories = new ConcurrentDictionary<NetworkPacketType, Func<object, byte[]>>();
+        _packetDeserializerFactories = new ConcurrentDictionary<NetworkPacketType, Func<byte[], object>>();
         
         // Base Packet Serializers
         Serializer.PrepareSerializer<NetworkPacket>();
@@ -24,15 +27,41 @@ public class PacketGenerator
         Serializer.PrepareSerializer<CRequestCryptoKeyPacket>();
     }
     
-    public void RegisterPackets()
+    public void RegisterPacketDeserializers()
     {
-        
-        _packetFactories.TryAdd(NetworkPacketType.CMSG_REQUEST_ENCRYPTION_KEY, SerializeCRequestCryptoKeyPacket);
-        _packetFactories.TryAdd(NetworkPacketType.SMSG_ENCRYPTION_KEY, SerializeSCryptoKeyPacket);
+        var packetTypes = typeof(PacketGenerator).Assembly.GetTypes()
+            .Where(type =>
+                type.IsClass &&
+                type.GetFields(BindingFlags.Public | BindingFlags.Static)
+                    .Any(field => field.FieldType == typeof(NetworkPacketType))
+            );
+
+        foreach (var packetType in packetTypes)
+        {
+            var serializerType = typeof(Serializer);
+            var genericPrepareSerializerMethod = serializerType.GetMethods()
+                .Single(m => m is { Name: "PrepareSerializer", IsGenericMethod: true } && m.GetParameters().Length == 0);
+
+            var closedPrepareSerializerMethod = genericPrepareSerializerMethod.MakeGenericMethod(packetType);
+            closedPrepareSerializerMethod.Invoke(null, null);
+            
+            var packetTypeValue = packetType.GetField(PacketTypeFieldName, BindingFlags.Public | BindingFlags.Static)
+                ?.GetValue(null);
+
+            if (packetTypeValue is NetworkPacketType pType)
+            {
+                _packetDeserializerFactories.TryAdd(pType, InternalDeserialization);
+            }
+        }
     }
-    
-    public object Deserialize(NetworkPacketType packetType, byte[] data)
+
+    private object InternalDeserialization(byte[] arg)
     {
-        return _packetFactories[packetType](data);
+        return Serializer.Deserialize<object>(new MemoryStream(arg));
+    }
+
+    public T Deserialize<T>(NetworkPacketType packetType, byte[] data) where T : class
+    {
+        return _packetDeserializerFactories[packetType](data) as T ?? throw new InvalidOperationException("Packet deserialization failed.");
     }
 }
