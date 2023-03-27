b260a99627c8e784aea2c1c183abb2c6561422d5
Nuno Silva <nuno.levezinho@live.com.pt>
2023-03-27 17:23:35 +0100

    Added more logic for the PacketGenerator registration

--- Avalon.Network.Packets/Auth/CAuthPacket.cs
+++ Avalon.Network.Packets/Auth/CAuthPacket.cs
@@ -5,6 +5,7 @@ namespace Avalon.Network.Packets.Auth;
 [ProtoContract]
 public class CAuthPacket
 {
+    public static NetworkPacketType PacketType = NetworkPacketType.CMSG_AUTH;
     [ProtoMember(1)] public string Username { get; set; }
     [ProtoMember(2)] public string Password { get; set; }
 }
