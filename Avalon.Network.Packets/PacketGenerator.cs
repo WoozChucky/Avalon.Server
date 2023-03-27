6aa0517748cc598369d332f9b04bf5964c49d65a
Nuno Levezinho <nuno.levezinho@live.com.pt>
2023-03-27 15:56:02 +0100

    WIP

--- Avalon.Network.Packets/PacketGenerator.cs
+++ Avalon.Network.Packets/PacketGenerator.cs
@@ -7,10 +7,12 @@ namespace Avalon.Network.Packets;
 public class PacketGenerator
 {
     
-    private readonly ConcurrentDictionary<NetworkPacketType, Func<object>> _packetFactories;
+    private readonly ConcurrentDictionary<NetworkPacketType, Func<object, byte[]>> _packetFactories;
 
     public PacketGenerator()
     {
+        _packetFactories = new ConcurrentDictionary<NetworkPacketType, Func<object, byte[]>>();
+        
         // Base Packet Serializers
         Serializer.PrepareSerializer<NetworkPacket>();
         Serializer.PrepareSerializer<NetworkPacketHeader>();
@@ -20,5 +22,17 @@ public class PacketGenerator
         
         // Client Packet Serializers
         Serializer.PrepareSerializer<CRequestCryptoKeyPacket>();
+    }
+    
+    public void RegisterPackets()
+    {
+        
+        _packetFactories.TryAdd(NetworkPacketType.CMSG_REQUEST_ENCRYPTION_KEY, SerializeCRequestCryptoKeyPacket);
+        _packetFactories.TryAdd(NetworkPacketType.SMSG_ENCRYPTION_KEY, SerializeSCryptoKeyPacket);
+    }
+    
+    public object Deserialize(NetworkPacketType packetType, byte[] data)
+    {
+        return _packetFactories[packetType](data);
     }
 }
