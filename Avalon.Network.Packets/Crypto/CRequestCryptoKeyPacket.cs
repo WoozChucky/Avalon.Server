df9b8dd80d7434470f924c8e85a69bda4c43562a
Nuno Levezinho <nuno.levezinho@live.com.pt>
2023-03-27 15:56:02 +0100

    WIP

--- Avalon.Network.Packets/Crypto/CRequestCryptoKeyPacket.cs
+++ Avalon.Network.Packets/Crypto/CRequestCryptoKeyPacket.cs
@@ -5,5 +5,5 @@ namespace Avalon.Network.Packets.Crypto;
 [ProtoContract]
 public class CRequestCryptoKeyPacket
 {
-    
+    public static NetworkPacketType PacketType = NetworkPacketType.CMSG_REQUEST_ENCRYPTION_KEY;
 }
