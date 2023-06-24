1d98c6843bc9c5b83856ad17344535366b39c748
Nuno Silva <nuno.levezinho@live.com.pt>
2023-06-24 22:52:42 +0100

    More work on map server side architecture

--- src/Server/Avalon.Map/Tile.cs
+++ src/Server/Avalon.Map/Tile.cs
@@ -1,7 +1,20 @@
+using System.Drawing;
+using System.Numerics;
+using Avalon.Common.Extensions;
+
 namespace Avalon.Map
 {
     public class Tile
     {
+        public Vector2 Position { get; private set; }
+        public Rectangle Bounds { get; private set; }
+        public Size Size { get; private set; }
         
+        public Tile(int x, int y, int size)
+        {
+            Position = new Vector2(x, y);
+            Size = new Size(size, size);
+            Bounds = new Rectangle(Position.ToPoint(), Size);
+        }
     }
 }
