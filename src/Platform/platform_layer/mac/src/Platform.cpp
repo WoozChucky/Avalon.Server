98df58e27bd3f3e7973077937a0c205eda0639d6
Nuno Silva <nuno.levezinho@live.com.pt>
2023-07-11 02:55:22 +0100

    Added macOS starting files

--- src/Platform/platform_layer/mac/src/Platform.cpp
+++ src/Platform/platform_layer/mac/src/Platform.cpp
@@ -9,27 +9,170 @@
 #include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
 #include <QuartzCore/QuartzCore.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
+
+#include "MyAppDelegate.hpp"
+#include "MyMTKViewDelegate.hpp"
+#include "Renderer.hpp"
+
+MyAppDelegate::~MyAppDelegate()
+{
+    _pMtkView->release();
+    _pWindow->release();
+    _pDevice->release();
+    delete _pViewDelegate;
+}
+
+NS::Menu* MyAppDelegate::createMenuBar()
+{
+    using NS::StringEncoding::UTF8StringEncoding;
+
+    NS::Menu* pMainMenu = NS::Menu::alloc()->init();
+    NS::MenuItem* pAppMenuItem = NS::MenuItem::alloc()->init();
+    NS::Menu* pAppMenu = NS::Menu::alloc()->init( NS::String::string( "Appname", UTF8StringEncoding ) );
+
+    NS::String* appName = NS::RunningApplication::currentApplication()->localizedName();
+    NS::String* quitItemName = NS::String::string( "Quit ", UTF8StringEncoding )->stringByAppendingString( appName );
+    SEL quitCb = NS::MenuItem::registerActionCallback( "appQuit", [](void*,SEL,const NS::Object* pSender){
+        auto pApp = NS::Application::sharedApplication();
+        pApp->terminate( pSender );
+    } );
+
+    NS::MenuItem* pAppQuitItem = pAppMenu->addItem( quitItemName, quitCb, NS::String::string( "q", UTF8StringEncoding ) );
+    pAppQuitItem->setKeyEquivalentModifierMask( NS::EventModifierFlagCommand );
+    pAppMenuItem->setSubmenu( pAppMenu );
+
+    NS::MenuItem* pWindowMenuItem = NS::MenuItem::alloc()->init();
+    NS::Menu* pWindowMenu = NS::Menu::alloc()->init( NS::String::string( "Window", UTF8StringEncoding ) );
+
+    SEL closeWindowCb = NS::MenuItem::registerActionCallback( "windowClose", [](void*, SEL, const NS::Object*){
+        auto pApp = NS::Application::sharedApplication();
+        pApp->windows()->object< NS::Window >(0)->close();
+    } );
+    NS::MenuItem* pCloseWindowItem = pWindowMenu->addItem( NS::String::string( "Close Window", UTF8StringEncoding ), closeWindowCb, NS::String::string( "w", UTF8StringEncoding ) );
+    pCloseWindowItem->setKeyEquivalentModifierMask( NS::EventModifierFlagCommand );
+
+    pWindowMenuItem->setSubmenu( pWindowMenu );
+
+    pMainMenu->addItem( pAppMenuItem );
+    pMainMenu->addItem( pWindowMenuItem );
+
+    pAppMenuItem->release();
+    pWindowMenuItem->release();
+    pAppMenu->release();
+    pWindowMenu->release();
+
+    return pMainMenu->autorelease();
+}
+
+void MyAppDelegate::applicationWillFinishLaunching( NS::Notification* pNotification )
+{
+    NS::Menu* pMenu = createMenuBar();
+    NS::Application* pApp = reinterpret_cast< NS::Application* >( pNotification->object() );
+    pApp->setMainMenu( pMenu );
+    pApp->setActivationPolicy( NS::ActivationPolicy::ActivationPolicyRegular );
+}
+
+void MyAppDelegate::applicationDidFinishLaunching( NS::Notification* pNotification )
+{
+    CGRect frame = (CGRect){ {100.0, 100.0}, {512.0, 512.0} };
+
+    _pWindow = NS::Window::alloc()->init(
+            frame,
+            NS::WindowStyleMaskClosable|NS::WindowStyleMaskTitled,
+            NS::BackingStoreBuffered,
+            false );
+
+    _pDevice = MTL::CreateSystemDefaultDevice();
+
+    _pMtkView = MTK::View::alloc()->init( frame, _pDevice );
+    _pMtkView->setColorPixelFormat( MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB );
+    _pMtkView->setClearColor( MTL::ClearColor::Make( 1.0, 0.0, 0.0, 1.0 ) );
+
+    _pViewDelegate = new MyMTKViewDelegate( _pDevice );
+    _pMtkView->setDelegate( _pViewDelegate );
+
+    _pWindow->setContentView( _pMtkView );
+    _pWindow->setTitle( NS::String::string( "00 - Window", NS::StringEncoding::UTF8StringEncoding ) );
+
+    _pWindow->makeKeyAndOrderFront( nullptr );
+
+    NS::Application* pApp = reinterpret_cast< NS::Application* >( pNotification->object() );
+    pApp->activateIgnoringOtherApps( true );
+}
+
+bool MyAppDelegate::applicationShouldTerminateAfterLastWindowClosed( NS::Application* pSender )
+{
+    return true;
+}
 
 void Initialize()
 {
     // Initialize the platform layer
     std::cout << "Initializing the platform layer" << std::endl;
 
-    //initialize autoreleasePool pool
-    NS::AutoreleasePool* poolAllocator = NS::AutoreleasePool::alloc()->init();
+    NS::AutoreleasePool* pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
 
-    //application delegate
-    core::Application game;
+    const MyAppDelegate del;
 
-    /* every application uses a single instance of NSApplication, to control the main event loop,
-     * keep track of app's windows and menus, distribute events to appropriate objects, set up
-     * autorelease pools, and receive notification of app-level events. */
-    NS::Application* appNS = NS::Application::sharedApplication();
+    NS::Application* pSharedApplication = NS::Application::sharedApplication();
+    pSharedApplication->setDelegate( &del );
+    pSharedApplication->run();
 
-    /* Every NSApplication has a delegate that is notified when app is start or terminated.
-     * By setting the delegate and implementing the delegate methods,
-     * you customize the behavior of your app without having to subclass NSApplication  */
-    appNS->setDelegate(&game);
-    appNS->run();
-    poolAllocator->release();
+    pAutoreleasePool->release();
 }
+
+
+#pragma mark - ViewDelegate
+#pragma region ViewDelegate {
+
+MyMTKViewDelegate::MyMTKViewDelegate( MTL::Device* pDevice )
+        : MTK::ViewDelegate()
+        , _pRenderer( new Renderer( pDevice ) )
+{
+}
+
+MyMTKViewDelegate::~MyMTKViewDelegate()
+{
+    delete _pRenderer;
+}
+
+void MyMTKViewDelegate::drawInMTKView( MTK::View* pView )
+{
+    _pRenderer->draw( pView );
+}
+
+#pragma endregion ViewDelegate }
+
+
+#pragma mark - Renderer
+#pragma region Renderer {
+
+Renderer::Renderer( MTL::Device* pDevice )
+        : _pDevice( pDevice->retain() )
+{
+    _pCommandQueue = _pDevice->newCommandQueue();
+}
+
+Renderer::~Renderer()
+{
+    _pCommandQueue->release();
+    _pDevice->release();
+}
+
+void Renderer::draw( MTK::View* pView )
+{
+    NS::AutoreleasePool* pPool = NS::AutoreleasePool::alloc()->init();
+
+    MTL::CommandBuffer* pCmd = _pCommandQueue->commandBuffer();
+    MTL::RenderPassDescriptor* pRpd = pView->currentRenderPassDescriptor();
+    MTL::RenderCommandEncoder* pEnc = pCmd->renderCommandEncoder( pRpd );
+    pEnc->endEncoding();
+    pCmd->presentDrawable( pView->currentDrawable() );
+    pCmd->commit();
+
+    pPool->release();
+}
+
+#pragma endregion Renderer }
