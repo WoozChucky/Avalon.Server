0c21c4fdf201757e9ffd80501742f6ae4e2522ce
Nuno Silva <nuno.levezinho@live.com.pt>
2023-08-21 01:12:47 +0100

    Added macOS renderer

--- src/Platform/platform_layer/mac/src/Renderer.hpp
+++ src/Platform/platform_layer/mac/src/Renderer.hpp
@@ -9,9 +9,16 @@ public:
     Renderer( MTL::Device* pDevice );
     ~Renderer();
 
+    void buildShaders();
+    void buildBuffers();
     void draw( MTK::View* pView );
 
 private:
     MTL::Device* _pDevice;
     MTL::CommandQueue* _pCommandQueue;
+    MTL::Library* _pShaderLibrary;
+    MTL::RenderPipelineState* _pPSO;
+    MTL::Buffer* _pArgBuffer;
+    MTL::Buffer* _pVertexPositionsBuffer;
+    MTL::Buffer* _pVertexColorsBuffer;
 };
