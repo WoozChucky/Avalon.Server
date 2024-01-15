fcc3a7d70354468904c8ed47c86cf954bbadfbd6
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-01-15 11:04:52 +0000

    Fix issue with wrong attribute being used

--- src/Server/Avalon.Database/Column.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Avalon.Database;
-
-[AttributeUsage(AttributeTargets.Property)]
-public class Column(string name) : Attribute
-{
-    public string Name { get; } = name;
-}
