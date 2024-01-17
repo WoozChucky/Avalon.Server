defac2d29a76170cda80cbd3612da30e1ceb317d
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-01-17 18:53:45 +0000

    feat(api): Added policies for rbac

---
+++ src/Server/Avalon.Api/Authentication/AvalonRoles.cs
@@ -0,0 +1,9 @@
+namespace Avalon.Api.Authentication;
+
+public class AvalonRoles
+{
+    public const string Console = "Console";
+    public const string Admin = "Admin";
+    public const string GameMaster = "GameMaster";
+    public const string Player = "Player";
+}
