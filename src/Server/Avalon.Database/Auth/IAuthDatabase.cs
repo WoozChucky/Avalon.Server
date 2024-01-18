bfd2a9fdcf96139fd93f5d7437c5111d6e4cef20
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-01-18 18:34:00 +0000

    feat(db): Fix null timestamp conversion from MariaDb to .NET code

--- src/Server/Avalon.Database/Auth/IAuthDatabase.cs
+++ src/Server/Avalon.Database/Auth/IAuthDatabase.cs
@@ -13,7 +13,13 @@ namespace Avalon.Database.Auth
         
         public AuthDatabase(DatabaseConfiguration configuration)
         {
-            var connectionString = $"Server={configuration.Auth.Host}; Port={configuration.Auth.Port}; Database={configuration.Auth.Database}; userid={configuration.Auth.Username}; Pwd={configuration.Auth.Password};";
+            var connectionString = $"Server={configuration.Auth.Host};" +
+                                   $"Port={configuration.Auth.Port};" +
+                                   $"Database={configuration.Auth.Database};" +
+                                   $"userid={configuration.Auth.Username};" +
+                                   $"Pwd={configuration.Auth.Password};" +
+                                   $"ConvertZeroDatetime=True;" +
+                                   $"AllowZeroDateTime=True";
             
             Account = new AccountTable(connectionString);
         }
