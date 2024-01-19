420c58192e5ceaceb07d546b87ea3d595a063b10
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-01-19 04:36:32 +0000

    feat(db): Changed class visibility for migrator project

--- src/Server/Avalon.Database.Migrator/Model/MigrationRecord.cs
+++ src/Server/Avalon.Database.Migrator/Model/MigrationRecord.cs
@@ -1,12 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Avalon.Database.Migrator.Model;
 
-public class MigrationRecord
+internal class MigrationRecord
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public byte[] Hash { get; set; }
     public string ExecutedBy { get; set; }
     public DateTime ExecutedOn { get; set; }
+}
+
+internal class MigrationRecordComparer : IEqualityComparer<MigrationRecord>
+{
+    public bool Equals(MigrationRecord x, MigrationRecord y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (ReferenceEquals(x, null) || ReferenceEquals(y, null)) return false;
+        if (x.GetType() != y.GetType()) return false;
+
+        return x.Name == y.Name && x.Hash.SequenceEqual(y.Hash);
+    }
+
+    public int GetHashCode(MigrationRecord obj)
+    {
+        unchecked
+        {
+            var hash = 17;
+            hash = hash * 23 + obj.Name.GetHashCode();
+
+            foreach (var b in obj.Hash)
+            {
+                hash = hash * 23 + b.GetHashCode();
+            }
+
+            return hash;
+        }
+    }
 }
