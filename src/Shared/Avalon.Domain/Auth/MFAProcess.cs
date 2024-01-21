11db6b86b4a1b16ef07dde9c2d6bfb2986193828
Nuno Silva <nuno@quix.io>
2024-01-21 16:34:11 +0000

    feat(api): Improved implementation of mfa setup

--- src/Shared/Avalon.Domain/Auth/MFAProcess.cs
+++ src/Shared/Avalon.Domain/Auth/MFAProcess.cs
@@ -1,7 +1,30 @@
+using Avalon.Domain.Attributes;
+
 namespace Avalon.Domain.Auth;
 
-public class MFAProcess {
-    public Guid Id { get; set; }
+public class MFASetup {
+    [Column("Id")]
+    public Guid? Id { get; set; }
+    [Column("AccountId")]
     public int AccountId { get; set; }
-    
+    [Column("Secret")]
+    public byte[] Secret { get; set; }
+    [Column("RecoveryCode1")]
+    public byte[] RecoveryCode1 { get; set; }
+    [Column("RecoveryCode2")]
+    public byte[] RecoveryCode2 { get; set; }
+    [Column("RecoveryCode3")]
+    public byte[] RecoveryCode3 { get; set; }
+    [Column("Status")]
+    public Status Status { get; set; }
+    [Column("CreatedAt")]
+    public DateTime CreatedAt { get; set; }
+    [Column("ConfirmedAt")]
+    public DateTime? ConfirmedAt { get; set; }
+}
+
+public enum Status : ushort {
+    Confirmed = 0,
+    Setup = 1,
+    Reset = 2
 }
