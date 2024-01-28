a611e2bb1f8454bab2010814aeb9d39e0393f619
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-01-28 05:12:00 +0000

    bug(db): Added default metadata for devices

--- src/Shared/Avalon.Domain/Auth/Device.cs
+++ src/Shared/Avalon.Domain/Auth/Device.cs
@@ -5,7 +5,7 @@ public class Device
     public int? Id { get; set; }
     public int AccountId { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Metadata { get; set; } = string.Empty;
+    public string Metadata { get; set; } = "{}";
     public bool Trusted { get; set; }
     public DateTime TrustEnd { get; set; }
     public DateTime LastUsage { get; set; }
