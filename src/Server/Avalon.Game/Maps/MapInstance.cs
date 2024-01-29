b8a43b9cfcd4f4d14b8389a31fb095830ed06a89
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-01-29 01:28:33 +0000

    feat(game): Changed the way we send packets by using a player radius (LOS)

--- src/Server/Avalon.Game/Maps/MapInstance.cs
+++ src/Server/Avalon.Game/Maps/MapInstance.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Drawing;
 using Avalon.Domain.World;
 using Avalon.Game.Creatures;
 using Avalon.Game.Maps.Virtual;
@@ -66,57 +67,130 @@ public class MapInstance
     public async void Update(TimeSpan deltaTime)
     {
         
-        // Broadcast Creature positions
-        foreach (var session in Sessions)
-        {
-            
-            if (!session.Value.InMap || session.Value.Status != ConnectionStatus.Connected) continue;
-            
-            // Creatures
-            var creatures = Creatures.Values
-                .Select(c => new CreaturePacket
-                {
-                    Id = c.Id,
-                    Name = c.Name,
-                    PositionX = c.Position.X,
-                    PositionY = c.Position.Y,
-                    VelocityX = c.Velocity.X,
-                    VelocityY = c.Velocity.Y
-                })
-                .ToArray();
-            
-            await session.Value.SendAsync(SNpcUpdatePacket.Create(
-                creatures,
-                session.Value.Encrypt));
-        }
+        // Currently we're going trough every session (player) and for each session, we send a packet for every other
+        // player in the map, another packet for every creature in the map and another packet for every event in the map.
+        // This is not optimal, but it's a good start.
+        // Now what i'm going to implement is a way send a less packets with information about referred entities but in a 
+        // radius of the player. This way we can reduce the amount of packets sent and the amount of data sent.
+        // Also the second step is to implement a way to send packets with grouped by entities
+        // (meaning a packet with a group of players, another packet with creatures and another with events).
         
+        const int radius = 10;
+
         // Broadcast player positions
+        
         foreach (var session in Sessions)
         {
             if (!session.Value.InMap || session.Value.Status != ConnectionStatus.Connected) continue;
-
-            var playerPackets = new List<SPlayerPacket>();
             
+            
+            var playerRectangle = new Rectangle(
+                (int)session.Value.Character!.PositionX,
+                (int)session.Value.Character!.PositionY,
+                32,
+                32);
+            
+            int centerX = playerRectangle.X + playerRectangle.Width / 2;
+            int centerY = playerRectangle.Y + playerRectangle.Height / 2;
+            
+            int circleDiameter = radius * 2;
+            int circleX = centerX - radius;
+            int circleY = centerY - radius;
+            
+            var playerRadiusRectangle = new Rectangle(
+                circleX,
+                circleY,
+                circleDiameter,
+                circleDiameter);
+            
+            var playerPackets = new List<SPlayerPacket>();
+
             foreach (var otherSession in Sessions)
             {
-                if (otherSession.Value == null || !otherSession.Value.InMap || otherSession.Value.Status != ConnectionStatus.Connected) continue;
+                if (!otherSession.Value.InMap || otherSession.Value.Status != ConnectionStatus.Connected) continue;
+                if (otherSession.Value.AccountId == session.Value.AccountId) continue;
                 
-                playerPackets.Add(new SPlayerPacket
+                var otherPlayerRectangle = new Rectangle(
+                    (int)otherSession.Value.Character!.PositionX,
+                    (int)otherSession.Value.Character!.PositionY,
+                    32,
+                    32);
+
+                if (playerRadiusRectangle.IntersectsWith(otherPlayerRectangle))
                 {
-                    AccountId = otherSession.Value.AccountId,
-                    CharacterId = otherSession.Value.Character!.Id!.Value,
-                    PositionX = otherSession.Value.Character.Movement.Position.X,
-                    PositionY = otherSession.Value.Character.Movement.Position.Y,
-                    VelocityX = otherSession.Value.Character.Movement.Velocity.X,
-                    VelocityY = otherSession.Value.Character.Movement.Velocity.Y,
-                    Chatting = otherSession.Value.Character!.IsChatting,
-                    Elapsed = otherSession.Value.Character.ElapsedGameTime
-                });
+                    //TODO: Add this player to the list of players to send to the current player
+                    playerPackets.Add(new SPlayerPacket
+                    {
+                        AccountId = otherSession.Value.AccountId,
+                        CharacterId = otherSession.Value.Character!.Id!.Value,
+                        PositionX = otherSession.Value.Character.Movement.Position.X,
+                        PositionY = otherSession.Value.Character.Movement.Position.Y,
+                        VelocityX = otherSession.Value.Character.Movement.Velocity.X,
+                        VelocityY = otherSession.Value.Character.Movement.Velocity.Y,
+                        Chatting = otherSession.Value.Character!.IsChatting,
+                        Elapsed = otherSession.Value.Character.ElapsedGameTime
+                    });
+                }
             }
             
             if (playerPackets.Count == 0) continue;
-
+            
             await session.Value.SendAsync(SPlayerPositionUpdatePacket.Create(playerPackets.ToArray(), session.Value.Encrypt));
+        }
+        
+        // Broadcast Creature positions
+        foreach (var session in Sessions)
+        {
+            if (!session.Value.InMap || session.Value.Status != ConnectionStatus.Connected) continue;
+            
+            var playerRectangle = new Rectangle(
+                (int)session.Value.Character!.PositionX,
+                (int)session.Value.Character!.PositionY,
+                32,
+                32);
+            
+            int centerX = playerRectangle.X + playerRectangle.Width / 2;
+            int centerY = playerRectangle.Y + playerRectangle.Height / 2;
+            
+            int circleDiameter = radius * 2;
+            int circleX = centerX - radius;
+            int circleY = centerY - radius;
+            
+            var playerRadiusRectangle = new Rectangle(
+                circleX,
+                circleY,
+                circleDiameter,
+                circleDiameter);
+            
+            var creaturePackets = new List<CreaturePacket>();
+
+            foreach (var creature in Creatures.Values)
+            {
+                var creatureRectangle = new Rectangle(
+                    (int) creature.Position.X,
+                    (int) creature.Position.Y,
+                    32,
+                    32);
+
+                if (playerRadiusRectangle.IntersectsWith(creatureRectangle))
+                {
+                    creaturePackets.Add(new CreaturePacket
+                    {
+                        Id = creature.Id,
+                        Name = creature.Name,
+                        PositionX = creature.Position.X,
+                        PositionY = creature.Position.Y,
+                        VelocityX = creature.Velocity.X,
+                        VelocityY = creature.Velocity.Y
+                    });
+                }
+            }
+            
+            if (creaturePackets.Count == 0) continue;
+            
+            await session.Value.SendAsync(SNpcUpdatePacket.Create(
+                creaturePackets.ToArray(),
+                session.Value.Encrypt));
         }
     }
 
