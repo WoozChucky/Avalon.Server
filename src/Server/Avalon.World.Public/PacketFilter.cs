71d8f4cd8eb56ae6b0bb085d92b6c6e5d15ba8f4
Nuno Silva <nuno.levezinho@live.com.pt>
2024-10-20 21:20:03 +0100

    docs: Generate cs docs for some of the public api

--- src/Server/Avalon.World.Public/PacketFilter.cs
+++ src/Server/Avalon.World.Public/PacketFilter.cs
@@ -5,9 +5,22 @@ using Avalon.Network.Packets.Abstractions;
 
 namespace Avalon.World.Public;
 
+/// <summary>
+///     Represents an abstract base class for packet filters.
+/// </summary>
 public abstract class PacketFilter
 {
+    /// <summary>
+    ///     Processes the specified network packet.
+    /// </summary>
+    /// <param name="packet">The network packet to process.</param>
+    /// <returns>true if the packet was successfully processed; otherwise, false.</returns>
     public abstract bool Process(NetworkPacket packet);
 
+    /// <summary>
+    ///     Determines whether the packet filter can process the specified packet type.
+    /// </summary>
+    /// <param name="type">The type of the network packet.</param>
+    /// <returns>true if the packet filter can process the specified packet type; otherwise, false.</returns>
     public abstract bool CanProcess(NetworkPacketType type);
 }
