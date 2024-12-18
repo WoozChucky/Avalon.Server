15e22bf62e85dc4fbcd63a08397c2da8c68a26c7
Nuno Silva <nuno.levezinho@live.com.pt>
2024-12-18 15:38:27 +0000

    Added submodule vendor/RaylibCS

---
+++ src/Client/Avalon.Client.Raylib/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Licensed to the Avalon MMORPG Game under one or more agreements.
+// Avalon MMORPG Game licenses this file to you under the MIT license.
+
+namespace Avalon.Client.Raylib;
+
+public class GlobalUsings
+{
+    
+}
