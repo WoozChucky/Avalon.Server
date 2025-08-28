277dbcbbb63b369fa7ee36c1170315a41a4fc3eb
Nuno Silva <nuno.levezinho@live.com.pt>
2025-08-28 22:49:53 +0100

    Remove dead code

--- src/Server/Avalon.Database/Extensions/ServiceCollectionExtensions.cs
+++ src/Server/Avalon.Database/Extensions/ServiceCollectionExtensions.cs
@@ -5,23 +5,12 @@ namespace Avalon.Database.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddAvalonDatabases(this IServiceCollection services, string configurationSection = "Database")
+    public static IServiceCollection AddAvalonDatabases(this IServiceCollection services,
+        string configurationSection = "Database")
     {
         services.AddOptions<DatabaseConfiguration>()
             .BindConfiguration(configurationSection);
 
         return services;
-    }
-
-    private static string GenerateConnectionString(DatabaseConnection configuration)
-    {
-        var connectionString = $"Server={configuration.Host};" +
-                               $"Port={configuration.Port};" +
-                               $"Database={configuration.Database};" +
-                               $"userid={configuration.Username};" +
-                               $"Pwd={configuration.Password};" +
-                               $"ConvertZeroDatetime=True;" +
-                               $"AllowZeroDateTime=True";
-        return connectionString;
     }
 }
