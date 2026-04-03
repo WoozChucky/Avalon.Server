059dcfb5fa3c52ab8d865eaf846691d8d2a4890b
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-03 11:39:03 +0100

    chore: remove AutoMapper entirely

--- src/Server/Avalon.Api/Controllers/CharacterController.cs
+++ src/Server/Avalon.Api/Controllers/CharacterController.cs
@@ -1,6 +1,6 @@
-using AutoMapper;
 using Avalon.Api.Authentication;
 using Avalon.Api.Contract;
+using Avalon.Api.Contract.Mappers;
 using Avalon.Api.Services;
 using Avalon.Common.ValueObjects;
 using Microsoft.AspNetCore.Authorization;
@@ -15,29 +15,25 @@ public class CharacterController : BaseController
 {
     private readonly IAuthContext _authContext;
     private readonly ICharacterService _service;
-    private readonly IMapper _mapper;
 
-    public CharacterController(IAuthContext authContext, ICharacterService service, IMapper mapper)
+    public CharacterController(IAuthContext authContext, ICharacterService service)
     {
         _authContext = authContext;
         _service = service;
-        _mapper = mapper;
     }
 
     [HttpGet(Name = "GetCharacter")]
     public async Task<IList<CharacterDto>> GetAll()
     {
         var characters = await _service.GetAllCharacters(_authContext.Account?.Id ?? throw new Exception("Account not loaded"));
-        var mapped = _mapper.Map<IList<CharacterDto>>(characters);
-        return mapped;
+        return characters.Select(c => c.ToDto()).ToList();
     }
 
     [HttpGet("{id}", Name = "GetCharacterById")]
     public async Task<CharacterDto> GetById(CharacterId id)
     {
         var character = await _service.GetCharacterById(id);
-        var mapped = _mapper.Map<CharacterDto>(character);
-        return mapped;
+        return character.ToDto();
     }
 
 }
