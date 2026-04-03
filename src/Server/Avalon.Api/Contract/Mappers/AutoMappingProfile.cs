976ea61f4a67cbf7ccb9cfb749552dd39ff247e0
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-03 11:39:03 +0100

    chore: remove AutoMapper entirely

--- src/Server/Avalon.Api/Contract/Mappers/AutoMappingProfile.cs
+++ src/Server/Avalon.Api/Contract/Mappers/AutoMappingProfile.cs
@@ -1,14 +1,43 @@
-using AutoMapper;
 using Avalon.Domain.Auth;
 using Avalon.Domain.Characters;
 
 namespace Avalon.Api.Contract.Mappers;
 
-public class AutoMappingProfile : Profile
+public static class MappingExtensions
 {
-    public AutoMappingProfile()
+    public static AccountDto ToDto(this Account account) => new()
     {
-        CreateMap<Account, AccountDto>();
-        CreateMap<Character, CharacterDto>();
-    }
+        Id = account.Id,
+        Username = account.Username,
+        Email = account.Email,
+        JoinDate = account.JoinDate,
+        LastIp = account.LastIp,
+        Locked = account.Locked,
+        MuteTime = account.MuteTime,
+        MuteReason = account.MuteReason,
+        Online = account.Online,
+        Locale = account.Locale,
+        Os = account.Os,
+        TotalTime = account.TotalTime,
+        AccessLevel = account.AccessLevel,
+    };
+
+    public static CharacterDto ToDto(this Character character) => new()
+    {
+        Id = character.Id,
+        Name = character.Name,
+        Class = character.Class,
+        Gender = character.Gender,
+        Level = character.Level,
+        Experience = character.Experience,
+        Map = character.Map,
+        Online = character.Online,
+        TotalTime = character.TotalTime,
+        TotalKills = character.TotalKills,
+        ChosenTitle = character.ChosenTitle,
+        Health = character.Health,
+        Latency = character.Latency,
+        CreationDate = character.CreationDate,
+        DeleteDate = character.DeleteDate,
+    };
 }
