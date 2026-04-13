6ad1c302f7a6ce1d5627f5fed7052f6a76fba0d0
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-13 23:29:41 +0100

    chore: remove dead code and mark legacy client as obsolete
    - Delete unreferenced AppConfiguration classes (Auth + World servers)
    - Delete unreferenced NetworkDaemonConfiguration
    - Mark AvalonTcpClient as legacy with [Obsolete] and a header comment
    - Remove TODO-014 from tracker (moot: client is retired, not wired up)
    - Remove stale LayerEnricher test for deprecated AvalonSslTcpServer namespace

--- src/Server/Avalon.Infrastructure/Configuration/NetworkDaemonConfiguration.cs
+++
@@ -1,8 +0,0 @@
-using Avalon.Network.Tcp.Configuration;
-
-namespace Avalon.Infrastructure.Configuration;
-
-public class NetworkDaemonConfiguration
-{
-    public AvalonTcpServerConfiguration? Tcp { get; set; }
-}
