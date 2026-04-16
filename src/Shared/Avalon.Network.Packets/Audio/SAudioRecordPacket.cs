21ecaa1d166deeec5ab8b55797eb36c6bbdcd93b
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-16 11:30:37 +0100

    refactor: migrate SAudioRecordPacket to PacketSerializationHelper

--- src/Shared/Avalon.Network.Packets/Audio/SAudioRecordPacket.cs
+++ src/Shared/Avalon.Network.Packets/Audio/SAudioRecordPacket.cs
@@ -13,29 +13,13 @@ public class SAudioRecordPacket : Packet
 
     [ProtoMember(1)] public byte[] SoundBuffer { get; set; }
 
-    public static NetworkPacket Create(byte[] soundBuffer, EncryptFunc? encryptFunc)
+    public static NetworkPacket Create(byte[] soundBuffer)
     {
-        using var memoryStream = new MemoryStream();
-
-        var packet = new SAudioRecordPacket()
+        var packet = new SAudioRecordPacket
         {
             SoundBuffer = soundBuffer
         };
 
-        Serializer.Serialize(memoryStream, packet);
-
-        var buffer = encryptFunc?.Invoke(memoryStream.ToArray()) ?? memoryStream.ToArray();
-
-        return new NetworkPacket
-        {
-            Header = new NetworkPacketHeader
-            {
-                Type = PacketType,
-                Flags = Flags,
-                Protocol = Protocol,
-                Version = 0
-            },
-            Payload = buffer
-        };
+        return PacketSerializationHelper.SerializeUnencrypted(packet, PacketType, Flags, Protocol);
     }
 }
