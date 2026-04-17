3053005fe91eea13671e77b91341965360691503
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-17 19:02:08 +0100

    fix: replace MethodInfo.Invoke with IPacketHandlerNew DIM dispatch in CallListener (GC-011)

--- src/Server/Avalon.Server.Auth/AuthPacketHandler.cs
+++ src/Server/Avalon.Server.Auth/AuthPacketHandler.cs
@@ -5,4 +5,7 @@ namespace Avalon.Server.Auth;
 public interface IAuthPacketHandler<T> : IPacketHandlerNew
 {
     Task ExecuteAsync(AuthPacketContext<T> ctx, CancellationToken token = default);
+
+    Task IPacketHandlerNew.ExecuteAsync(object context, CancellationToken token)
+        => ExecuteAsync((AuthPacketContext<T>)context, token);
 }
