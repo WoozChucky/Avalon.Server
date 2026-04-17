d70a07c606d687e9ec70ec02177d011667aea07c
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-17 19:02:08 +0100

    fix: replace MethodInfo.Invoke with IPacketHandlerNew DIM dispatch in CallListener (GC-011)

--- src/Server/Avalon.World/WorldPacketHandler.cs
+++ src/Server/Avalon.World/WorldPacketHandler.cs
@@ -5,4 +5,7 @@ namespace Avalon.World;
 public interface IWorldPacketHandler<T> : IPacketHandlerNew
 {
     Task ExecuteAsync(WorldPacketContext<T> ctx, CancellationToken token = default);
+
+    Task IPacketHandlerNew.ExecuteAsync(object context, CancellationToken token)
+        => ExecuteAsync((WorldPacketContext<T>)context, token);
 }
