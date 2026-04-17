2bee3f5f9338970ae3df426dbf5b2fdc4c5ea273
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-17 18:56:46 +0100

    refactor: delete dead IPacketHandler/IPacketRegistry/AvalonTcpClient legacy system

--- tests/Avalon.Server.World.UnitTests/InfrastructureFixture.cs
+++ tests/Avalon.Server.World.UnitTests/InfrastructureFixture.cs
@@ -2,7 +2,6 @@ using Avalon.Common.Cryptography;
 using Avalon.Infrastructure;
 using Avalon.Metrics;
 using Avalon.Network;
-using Avalon.Network.Packets.Internal;
 using Avalon.Network.Packets.Internal.Deserialization;
 using Avalon.Network.Packets.Serialization;
 using Avalon.World.Configuration;
@@ -12,23 +11,22 @@ namespace Avalon.Server.World.UnitTests;
 public class InfrastructureFixture : IDisposable
 {
 	public CancellationTokenSource CancellationTokenSource { get; set; }
-	
+
 	public IAvalonInfrastructure Infrastructure { get; set; }
 	public IReplicatedCache Cache { get; set; }
 	public IAvalonNetworkDaemon NetworkDaemon { get; set; }
 	public IMetricsManager MetricsManager { get; set; }
-	
+
 	public IAvalonTcpServer TcpServer { get; set; }
-	
+
 	public IPacketSerializer PacketSerializer { get; set; }
 	public IPacketDeserializer PacketDeserializer { get; set; }
-	public IPacketRegistry PacketRegistry { get; set; }
 	public ICryptoManager CryptoManager { get; set; }
-	
+
 	public GameConfiguration GameConfiguration { get; set; } = new();
-    
+
     public void Dispose()
     {
-	    
+
     }
 }
