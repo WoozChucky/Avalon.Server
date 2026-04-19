72d4b07c33b38c22b7d5ce47708dbab3e9e24c84
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-19 18:18:21 +0100

    chore: remove deprecated Avalon.Network and Avalon.Network.Tcp projects
    Deletes the legacy networking projects and their associated interfaces, configurations, and unit tests. This cleanup removes deprecated TCP server implementations in favor of the current ServerBase architecture and drops the Serilog LayerEnrichers that relied on these namespaces.

--- src/Shared/Avalon.Network/IAvalonTcpServer.cs
+++
@@ -1,8 +0,0 @@
-namespace Avalon.Network;
-
-public delegate void TcpClientConnectedHandler(object? sender, TcpClient client);
-
-public interface IAvalonTcpServer : IAvalonNetworkServer
-{
-    event TcpClientConnectedHandler ClientConnected;
-}
