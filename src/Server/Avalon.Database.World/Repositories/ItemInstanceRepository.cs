8af57741f61990311240945f6949f90133c3f73b
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-22 23:43:40 +0100

    feat(world-db): add GetByCharacterIdWithTemplateAsync to IItemInstanceRepository

--- src/Server/Avalon.Database.World/Repositories/ItemInstanceRepository.cs
+++ src/Server/Avalon.Database.World/Repositories/ItemInstanceRepository.cs
@@ -1,17 +1,32 @@
 using Avalon.Common.ValueObjects;
 using Avalon.Domain.World;
+using Microsoft.EntityFrameworkCore;
 
 namespace Avalon.Database.World.Repositories;
 
 public interface IItemInstanceRepository : IRepository<ItemInstance, ItemInstanceId>
 {
-
+    Task<IReadOnlyList<ItemInstance>> GetByCharacterIdWithTemplateAsync(
+        CharacterId characterId, CancellationToken cancellationToken = default);
 }
 
 public class ItemInstanceRepository : EntityFrameworkRepository<ItemInstance, ItemInstanceId>, IItemInstanceRepository
 {
+    private readonly WorldDbContext _dbContext;
+
     public ItemInstanceRepository(WorldDbContext dbContext)
         : base(dbContext)
     {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IReadOnlyList<ItemInstance>> GetByCharacterIdWithTemplateAsync(
+        CharacterId characterId, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.ItemInstances
+            .AsNoTracking()
+            .Include(x => x.Template)
+            .Where(x => x.CharacterId == characterId)
+            .ToListAsync(cancellationToken);
     }
 }
