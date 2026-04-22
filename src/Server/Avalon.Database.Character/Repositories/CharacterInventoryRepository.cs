2e8f8b26dc7f1157ac50411977a778c2d485a512
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-22 12:43:34 +0100

    refactor(db.character): thread CancellationToken through repositories

--- src/Server/Avalon.Database.Character/Repositories/CharacterInventoryRepository.cs
+++ src/Server/Avalon.Database.Character/Repositories/CharacterInventoryRepository.cs
@@ -6,10 +6,10 @@ namespace Avalon.Database.Character.Repositories;
 
 public interface ICharacterInventoryRepository
 {
-    Task<CharacterInventory> CreateAsync(CharacterInventory inventory);
-    Task<IList<CharacterInventory>> CreateAsync(IList<CharacterInventory> inventories);
-    Task<CharacterInventory> UpdateAsync(CharacterInventory inventory);
-    Task<IReadOnlyCollection<CharacterInventory>> GetByCharacterIdAsync(CharacterId characterId);
+    Task<CharacterInventory> CreateAsync(CharacterInventory inventory, CancellationToken cancellationToken = default);
+    Task<IList<CharacterInventory>> CreateAsync(IList<CharacterInventory> inventories, CancellationToken cancellationToken = default);
+    Task<CharacterInventory> UpdateAsync(CharacterInventory inventory, CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<CharacterInventory>> GetByCharacterIdAsync(CharacterId characterId, CancellationToken cancellationToken = default);
 }
 
 public class CharacterInventoryRepository : ICharacterInventoryRepository
@@ -21,38 +21,38 @@ public class CharacterInventoryRepository : ICharacterInventoryRepository
         _dbContext = dbContext;
     }
 
-    public async Task<CharacterInventory> CreateAsync(CharacterInventory inventory)
+    public async Task<CharacterInventory> CreateAsync(CharacterInventory inventory, CancellationToken cancellationToken = default)
     {
-        var entity = await _dbContext.CharacterInventory.AddAsync(inventory);
-        await _dbContext.SaveChangesAsync();
+        var entity = await _dbContext.CharacterInventory.AddAsync(inventory, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return entity.Entity;
     }
 
-    public async Task<IList<CharacterInventory>> CreateAsync(IList<CharacterInventory> inventories)
+    public async Task<IList<CharacterInventory>> CreateAsync(IList<CharacterInventory> inventories, CancellationToken cancellationToken = default)
     {
         var entityList = new List<CharacterInventory>();
         foreach (var inventory in inventories)
         {
-            var entity = await _dbContext.CharacterInventory.AddAsync(inventory);
+            var entity = await _dbContext.CharacterInventory.AddAsync(inventory, cancellationToken);
             entityList.Add(entity.Entity);
         }
-        await _dbContext.SaveChangesAsync();
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return entityList;
     }
 
-    public async Task<CharacterInventory> UpdateAsync(CharacterInventory inventory)
+    public async Task<CharacterInventory> UpdateAsync(CharacterInventory inventory, CancellationToken cancellationToken = default)
     {
         var entity = _dbContext.CharacterInventory.Update(inventory);
-        await _dbContext.SaveChangesAsync();
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return entity.Entity;
     }
 
-    public async Task<IReadOnlyCollection<CharacterInventory>> GetByCharacterIdAsync(CharacterId characterId)
+    public async Task<IReadOnlyCollection<CharacterInventory>> GetByCharacterIdAsync(CharacterId characterId, CancellationToken cancellationToken = default)
     {
         return await _dbContext.CharacterInventory
             .AsNoTracking()
             .Where(entity => entity.CharacterId == characterId)
-            .ToListAsync();
+            .ToListAsync(cancellationToken);
     }
 
 }
