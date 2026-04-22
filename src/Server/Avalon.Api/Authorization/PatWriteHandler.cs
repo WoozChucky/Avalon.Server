b3159aaef0fcddf3eb0ca0cdde4eae424e7d8c5f
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-22 06:18:40 +0100

    feat(api): PatReadHandler and PatWriteHandler resource authz

---
+++ src/Server/Avalon.Api/Authorization/PatWriteHandler.cs
@@ -0,0 +1,23 @@
+using Avalon.Api.Authentication;
+using Avalon.Domain.Auth;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Avalon.Api.Authorization;
+
+public sealed class PatWriteHandler : AuthorizationHandler<WriteRequirement, PersonalAccessToken>
+{
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context, WriteRequirement requirement, PersonalAccessToken resource)
+    {
+        if (context.User.HasRoleAtLeast(AvalonRoles.Admin))
+        {
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+
+        if (resource.AccountId == context.User.AccountId())
+            context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
+}
