a249fa0036ac86a4db775a810226c5db314047b8
Nuno Silva <nuno.levezinho@live.com.pt>
2026-04-24 15:40:27 +0100

    feat(db): seed ForestDungeon map template + Town portal

--- src/Server/Avalon.Database.World/Migrations/20260424143756_SeedForestDungeon.Designer.cs
+++ src/Server/Avalon.Database.World/Migrations/20260424143756_SeedForestDungeon.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Avalon.Database.World;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Avalon.Database.World.Migrations
 {
     [DbContext(typeof(WorldDbContext))]
-    partial class WorldDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260424143756_SeedForestDungeon")]
+    partial class SeedForestDungeon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Server/Avalon.Database.World/Migrations/20260424143756_SeedForestDungeon.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Avalon.Database.World.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedForestDungeon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MapPortals",
+                columns: new[] { "Id", "Radius", "SourceMapId", "TargetMapId", "X", "Y", "Z" },
+                values: new object[] { 1, 3f, 1, 2, 50f, 51f, 50f });
+
+            migrationBuilder.InsertData(
+                table: "MapTemplates",
+                columns: new[] { "Id", "AreaTableId", "CorpseX", "CorpseY", "CorpseZ", "DefaultSpawnX", "DefaultSpawnY", "DefaultSpawnZ", "Description", "Directory", "LoadingScreenId", "LogoutMapId", "MapType", "MaxLevel", "MaxPlayers", "MinLevel", "Name", "PvP" },
+                values: new object[] { 2, 0, null, null, null, 0f, 0f, 0f, "Forest Dungeon", "Maps/", 0, 1, 1, 10, 1, 1, "ForestDungeon", false });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MapPortals",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MapTemplates",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
